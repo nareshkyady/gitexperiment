018a77bf397cfed8ea1609ab98523fff3912a496
nkydrwr <nareshk027@gmail.com>
2020-02-17 11:56:46 -0500

    first commit

---
+++ src/main/java/com/nkyd/gitexperiments/GitexperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.nkyd.gitexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexperimentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nkyd/gitexperiments/GitexperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkyd.gitexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
